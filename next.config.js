a6e88d4d11f2751bc55d5a165dba1c113ea41b31
fredlardo <80174024+fredlardo@users.noreply.github.com>
2026-07-07 21:37:05 +0100

    \ Fix Vercel deployment: switch SQLite -> PostgreSQL,move prisma to deps,add outputFileTracingIncludes\

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
       { protocol: 'https', hostname: '**' },
     ],
   },
+  outputFileTracingIncludes: {
+    '/*': ['./node_modules/.prisma/client/**/*'],
+    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
+  },
 }
 
 module.exports = nextConfig
